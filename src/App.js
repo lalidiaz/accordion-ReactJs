64db2d31ed542c5da289387412c938dd5df7a91e
Lali <lauradiaz1586@gmail.com>
2021-02-16 12:52:01 +0400

    add styles and toggle to show info of the topic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './components/Question';
+import Question from './components/Question';
 
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main className="container">
+      <div className="title-container">
+        <h3>Discover Waitomo Caves </h3>
+        <img
+          src="https://img.freepik.com/free-vector/questions-concept-illustration_114360-3932.jpg?size=338&ext=jpg&ga=GA1.2.1789656252.1612428479"
+          alt=""
+        />
+      </div>
+      <section className="info">
+        {questions.map((question) => {
+          return <Question key={question.id} {...question} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;
