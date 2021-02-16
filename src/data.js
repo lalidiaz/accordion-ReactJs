70b89c99c11b842052a2a65df3cf6cd5ccfc39e9
Lali <lauradiaz1586@gmail.com>
2021-02-16 11:57:29 +0400

    add database waitomo

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'World Famous in New Zealand: Waitomo Caves:',
+    info:
+      "There is nowhere in New Zealand like Waitomo Caves to see and experience the true subterranean beauty of the Earth. The Caves were created over 30 million years ago and offer some of the country's best natural history and beauty."
+  },
+  {
+    id: 2,
+    title: 'Where? What is it?',
+    info:
+      'The Waitomo Caves, in the North King Country, lets you explore the real underworld of New Zealand. It\'s a two-and-a-half hour drive from Auckland along SH1 and SH39. Waitomo in Maori translates to  "water which flows into a hole in the ground", and comprises a number of underground caving networks where one can explore.'
+  },
+  {
+    id: 3,
+    title: 'Why Go?',
+    info:
+      "Entering the Waitomo Caves is a real underground adventure - whether it's on foot, abseiling through a hole in the ground, or plunging butt-first in to the water. Under the Earth's surface, chambers of pure white limestone, stalactites and stalagmites are illuminated by glow worms. Almost every corner of the cave system's roofs and walls are adorned with the Earth's sparkling creations coloured in pale brown, pink and white. The caves aren't small and tight; rather quite the opposite. Water flowing the caverns for millions of years has built a series of chambers and cathedrals where even a choir and 600 people can enjoy the natural wonders of the Earth. The labyrinth of dark tunnels are lined with inky black water, where the glow worms' luminescent shine reflects like a night sky. Guided by a torchlight and glow worms, you can abseil, zip-line, go black water rafting, boat, climbwaterfalls and more, all - quite simply - underneath the Earth. You can take a walk through different cave systems, some dry and some wet or float through the underground caverns on rubber rings. A multitude of different caving organisations in Waitomo offer different experiences for each person"
+  },
+  {
+    id: 4,
+    title: 'Insider Tip',
+    info:
+      "Get to know the locals, especially the local kaumatua. They are the storehouses of tribal knowledge, know the history behind every scratch and surface of Waitomo and know the best places to go. Locals can tell you the stories of the haunted hotel, give you directions and tell you where the best local beer is brewed (Curly's Bar). They can even tell you about the best swimming spots in the region (the old quarry, 1638 Oparure Rd)."
+  },
+  {
+    id: 5,
+    title: 'Best time to go?',
+    info:
+      "The caves are underground so you can go any time of the year. The temperature inside ranges from 12-14C year round and the water temperature doesn't change a whole lot either. Although winter always has the best money-saving deals, spring is a good time to experience the region's other activities.  "
+  }
+];
+export default questions;
